b2979ade5e82740df168b3e8ad0fee796a2b0019
iisakov <iisakov.artisan@yandex.ru>
2023-10-10 10:31:05 +0300

    modified:   clients/telegram/types.go modified:   go.mod

--- clients/telegram/types.go
+++ clients/telegram/types.go
@@ -2,10 +2,10 @@ package telegram
 
 type UpdatesResponse struct {
 	OK     bool      `json:"ok"`
-	Result []Updates `json: "result"`
+	Result []Updates `json:"result"`
 }
 
 type Updates struct {
 	ID      int    `json:"update_id"`
-	Message string `json: "message"`
+	Message string `json:"message"`
 }
